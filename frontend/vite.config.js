e69cd11e8d9a0c0cb14f0ff171eb719b5ffee4d1
Abhigyan Jaiswal <abhigyanjj@gmail.com>
2026-01-18 12:33:51 +0900

    Fixed export pdf

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'html2canvas': resolve(__dirname, 'node_modules/html2canvas-pro'),
+    },
+  },
 })
